321829bfd49b49dfe9abd961e396a1b3b0218741
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-03-31 18:13:28 -0300

    pruebas de logica 2

--- api/router.js
+++ api/router.js
@@ -80,7 +80,7 @@ router.get('/success', success)
 
 
 //Products
-router.get('/api/renderProducts',  renderProducts)
+router.get('/api/renderProducts', passport.authenticate('session'),  renderProducts)
 router.post('/api/createProduct',  uploadSingle,   passport.authenticate('session'),  createProduct)
 router.delete('/api/deleteProduct/:id', deleteProduct)
 router.get('/api/detailsProduct/:id', detailsProduct)
