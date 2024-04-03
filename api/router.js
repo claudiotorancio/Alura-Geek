1a29298616338d6d5956ee1f7edf22060d1ed4e9
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-03 19:32:36 -0300

    pruebas de logica 49

--- api/router.js
+++ api/router.js
@@ -17,7 +17,7 @@ import deleteProduct from "../backend/routes/Prduct/deleteProduct.js";
 import detailsProduct from "../backend/routes/Prduct/detailsProduct.js";
 import updateProduct from "../backend/routes/Prduct/updateProduct.js";
 import path from 'path';
-import productoInicio from "../backend/routes/Prduct/productoInicio.js";
+
 
 const router = Router();
 
@@ -76,7 +76,7 @@ router.post('/api/signup', signup);
 router.post('/api/signin', signin);
 router.delete('/api/logout', logout)
 
-router.get('/api/createProduct', productoInicio)
+
 router.get('/api/renderInicio', renderInicio)
 router.get('/api/renderProducts', renderProducts);
 router.post('/api/createProduct',  createProduct);
