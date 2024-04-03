d011b950220bc3ecf5ed3c8cb35817778251c4e7
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-02 23:53:09 -0300

    pruebas de logica 36

--- frontend/app.js
+++ frontend/app.js
@@ -15,11 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const actualizarUsuario = document.querySelector('.data-user');
     const logoutUsuario = document.querySelector('[data-logOut]')
     const userActive = document.querySelector('[data-log]')
+    const cardActive = document.querySelector('data-card')
     if (user) {
         controllers.render();
         actualizarUsuario.textContent = `Hola! ${user}`
         logoutUsuario.textContent = 'Logout'
         userActive.style.display = 'none'
+        cardActive.style.display = 'none'
 
     } else {
         actualizarUsuario.style.display = 'none'
