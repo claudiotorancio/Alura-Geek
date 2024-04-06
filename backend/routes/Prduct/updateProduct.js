e91a0157d52e9dce98d093f1bdf327368ebea581
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-06 16:51:05 -0300

    actualzar imagen sin cambio 6

--- backend/routes/Prduct/updateProduct.js
+++ backend/routes/Prduct/updateProduct.js
@@ -18,28 +18,26 @@ const updateProduct = async (req, res) => {
       return res.status(401).json({ error: 'Usuario no autenticado' });
     }
 
+    // Parámetros del formulario
     const { id } = req.params;
     const { name, price } = req.body;
-    let imagePath = '';
-
-    if (req.file) {
-      imagePath = req.file.location;
-    } else {
-      const product = await Product.findById(id);
-      imagePath = product.imagePath;
-    }
-
+    let imagePath = req.file ? req.file.location : ''; // Si no hay nueva imagen, mantener la imagen anterior
+    const oldImagePath = req.body.oldImagePath; // Usar la ruta de la imagen anterior
+    
     const updateProduct = {
       name,
       price,
       imagePath,
+      oldImagePath,
     };
 
+    // Conectar a la base de datos mediante serverless function
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
+    // Crear un nuevo producto o vista según el rol del usuario
     let result;
     if (esAdministrador(req.user)) {
       result = await Vista.findByIdAndUpdate(id, updateProduct, { new: true });
@@ -47,39 +45,36 @@ const updateProduct = async (req, res) => {
       result = await Product.findByIdAndUpdate(id, updateProduct, { new: true });
     }
 
+    // Borrar la imagen anterior en S3 si se proporciona una nueva
+    if (req.file && oldImagePath) {
+      const nombreDeArchivo = oldImagePath.split("/").pop();
+      const params = {
+        Bucket: process.env.BUCKET_AWS,
+        Key: nombreDeArchivo,
+      };
+
+      s3.deleteObject(params, (err, data) => {
+        if (err) {
+          console.error("Error al eliminar la imagen anterior en S3:", err);
+        } else {
+          console.log("Imagen anterior eliminada con éxito en S3:", data);
+        }
+      });
+    }
+
     if (!result) {
-      return res.status(404).json({ message: "Product not found" });
+      res.status(404).json({ message: "Product not found" });
+    } else {
+      res.json({ message: "Product updated", updatedProduct: result });
     }
-
-    if (req.file && result.imagePath !== oldImagePath) {
-      deleteOldImageFromS3(result.imagePath);
-    }
-
-    return res.json({ message: "Product updated", updatedProduct: result });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 const esAdministrador = (user) => {
   return user.role === "admin";
-};
-
-const deleteOldImageFromS3 = (imagePath) => {
-  const nombreDeArchivo = imagePath.split("/").pop();
-  const params = {
-    Bucket: process.env.BUCKET_AWS,
-    Key: nombreDeArchivo,
-  };
-
-  s3.deleteObject(params, (err, data) => {
-    if (err) {
-      console.error('Error al eliminar la imagen en S3:', err);
-    } else {
-      console.log('Imagen anterior eliminada con éxito en S3:', data);
-    }
-  });
 };
 
 export default updateProduct;
