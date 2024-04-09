7ade58963be9bbc7b9a949c8abd998a624dc752f
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-08 23:58:38 -0300

    administrando a usuario admin

--- frontend/app.js
+++ frontend/app.js
@@ -10,15 +10,16 @@ import { listaControllers } from "./lista.controllers.js";
 
 // busqueda de cambios
 document.addEventListener("DOMContentLoaded", () => {
- 
+  listaControllers.renderLista()
+    controllers.render();
+  
   const user = JSON.parse(sessionStorage.getItem("user")) || null;
   const actualizarUsuario = document.querySelector(".data-user");
   const logoutUsuario = document.querySelector("[data-logOut]");
   const userActive = document.querySelector("[data-log]");
   const contactUser = document.querySelector("[data-contact]");
   if (user) {
-    listaControllers.renderLista()
-    controllers.render();
+  
     actualizarUsuario.textContent = `Hola! ${user}`;
     logoutUsuario.textContent = "Logout";
     userActive.style.display = "none";
