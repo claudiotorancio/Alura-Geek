a76d286a85f938119c0f5cf2f146dd9b9b5e6bf0
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-12 20:18:08 -0300

    mejorando logica para vista inicial 3

--- frontend/controllers_inicio.js
+++ frontend/controllers_inicio.js
@@ -2,8 +2,8 @@ import { productoServices } from "./servicios/product_services.js";
 import { controllers } from "./productos_controllers.js";
 
 const productoInicio = (description, name, imagePath) => {
-    const card = document.createElement("div");
-    const contenido = `
+  const card = document.createElement("div");
+  const contenido = `
   
           <div class="container mx-auto mt-4">
     
@@ -21,150 +21,146 @@ const productoInicio = (description, name, imagePath) => {
     
     </div>
       `;
-  
-    card.innerHTML = contenido;
-    card.classList.add('card');
-  
-    card.querySelector("a").addEventListener("click", (e) => {
-      e.preventDefault();
-      try {
-        controllers.mostrarProducto(imagePath, name, description);
-      } catch (err) {
-        console.log(err);
-      }
-    });
 
+  card.innerHTML = contenido;
+  card.classList.add("card");
 
-    return card;
-  };
-  
-
-  const renderInit = async () => {
+  card.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
     try {
-        const listaProductos = await productoServices.renderInicio();
-        const products = listaProductos;
-
-        // Objeto para almacenar los productos por sección
-        const productosPorSeccion = {
-            opcion1: [],
-            opcion2: [],
-            opcion3: []
-        };
-
-        // Dividir los productos por sección
-        products.forEach((elemento) => {
-            productosPorSeccion[elemento.section].push(elemento);
-        });
-
-        // Renderizar solo los primeros tres productos en cada sección por defecto
-        for (const [seccion, productos] of Object.entries(productosPorSeccion)) {
-            const contenedorProductos = document.querySelector(`[data-${seccion}]`);
-            contenedorProductos.innerHTML = ""; // Limpiar contenido existente
-
-            const primerosTresProductos = productos.slice(0, 3);
-            primerosTresProductos.forEach((producto) => {
-                contenedorProductos.appendChild(
-                    productoInicio(
-                        producto.description,
-                        producto.name,
-                        producto.imagePath,
-                        producto.price,
-                        producto._id
-                    )
-                );
-            });
-        }
-    } catch (error) {
-        console.error(error);
+      controllers.mostrarProducto(imagePath, name, description);
+    } catch (err) {
+      console.log(err);
     }
+  });
+
+  return card;
 };
 
+const renderInit = async () => {
+  try {
+    const listaProductos = await productoServices.renderInicio();
+    const products = listaProductos;
 
-document.querySelectorAll('.categoria').forEach(categoria => {
-  const categoriaBtn = categoria.querySelector('a');
-  const opcion = categoriaBtn.getAttribute('id');
+    // Objeto para almacenar los productos por sección
+    const productosPorSeccion = {
+      opcion1: [],
+      opcion2: [],
+      opcion3: [],
+    };
+
+    // Dividir los productos por sección
+    products.forEach((elemento) => {
+      productosPorSeccion[elemento.section].push(elemento);
+    });
+
+    // Renderizar solo los primeros tres productos en cada sección por defecto
+    for (const [seccion, productos] of Object.entries(productosPorSeccion)) {
+      const contenedorProductos = document.querySelector(`[data-${seccion}]`);
+      contenedorProductos.innerHTML = ""; // Limpiar contenido existente
+
+      const primerosTresProductos = productos.slice(0, 3);
+      primerosTresProductos.forEach((producto) => {
+        contenedorProductos.appendChild(
+          productoInicio(
+            producto.description,
+            producto.name,
+            producto.imagePath,
+            producto.price,
+            producto._id
+          )
+        );
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+document.querySelectorAll(".categoria").forEach((categoria) => {
+  const categoriaBtn = categoria.querySelector("a");
+  const opcion = categoriaBtn.getAttribute("id");
   const contenedorProductos = document.querySelector(`[data-${opcion}]`);
   let mostrarTodos = false; // Variable para mantener el estado de visualización
 
-  categoriaBtn.addEventListener('click', async (e) => {
-      e.preventDefault(); // Evitar comportamiento predeterminado del enlace
+  categoriaBtn.addEventListener("click", async (e) => {
+    e.preventDefault(); // Evitar comportamiento predeterminado del enlace
 
-      try {
-          // Si aún no se han mostrado todos los productos
-          if (!mostrarTodos) {
-              contenedorProductos.classList.add('allProducts');
+    try {
+      // Si aún no se han mostrado todos los productos
+      if (!mostrarTodos) {
+        contenedorProductos.classList.add("allProducts");
 
-              const respuesta = await productoServices.listaProductos();
-              const {usuarioHaIniciadoSesion} = respuesta // Acceder al arreglo de usuarios
- 
-              let render
+        const respuesta = await productoServices.listaProductos();
+        const { usuarioHaIniciadoSesion } = respuesta; // Acceder al arreglo de usuarios
 
-              if(!usuarioHaIniciadoSesion){
-                
-                render = productoInicio
+        let render;
 
-              } else {
-             
-                render = controllers.nuevoProducto
-              }
+        if (!usuarioHaIniciadoSesion) {
+          render = productoInicio;
+        } else {
+          render = controllers.nuevoProducto;
+        }
 
-              const listaProductos = await productoServices.renderInicio();
-              const products = listaProductos.filter(producto => producto.section === opcion);
+        const listaProductos = await productoServices.renderInicio();
+        const products = listaProductos.filter(
+          (producto) => producto.section === opcion
+        );
 
-              contenedorProductos.innerHTML = ""; // Limpiar contenido existente
-              products.forEach((producto) => {
-                  contenedorProductos.appendChild(
-                      render(
-                          producto.description,
-                          producto.name,
-                          producto.imagePath,
-                          producto.price,
-                          producto._id
-                      )
-                  );
-              });
+        contenedorProductos.innerHTML = ""; // Limpiar contenido existente
+        products.forEach((producto) => {
+          contenedorProductos.appendChild(
+            render(
+              
+              producto.name,
+              producto.price,
+              producto.imagePath,
+              producto.description,
+              producto._id
+            )
+          );
+        });
 
-              const tarjetas = contenedorProductos.querySelectorAll('.card');
-              tarjetas.forEach(tarjeta => {
-                  tarjeta.classList.add('allCard');
-              });
+        const tarjetas = contenedorProductos.querySelectorAll(".card");
+        tarjetas.forEach((tarjeta) => {
+          tarjeta.classList.add("allCard");
+        });
 
-              const imagen = contenedorProductos.querySelectorAll('.img-card');
-              imagen.forEach(tarjeta => {
-                  tarjeta.classList.add('img-allCard');
-              });
+        const imagen = contenedorProductos.querySelectorAll(".img-card");
+        imagen.forEach((tarjeta) => {
+          tarjeta.classList.add("img-allCard");
+        });
 
-              mostrarTodos = true; // Cambiar el estado a mostrar todos
+        mostrarTodos = true; // Cambiar el estado a mostrar todos
 
-              // Cambiar el texto del enlace a 'Volver'
-              categoriaBtn.textContent = 'Volver';
+        // Cambiar el texto del enlace a 'Volver'
+        categoriaBtn.textContent = "Volver";
 
-              // Limpiar contenido de las secciones diferentes a la actual
-              document.querySelectorAll(".productos").forEach(contenedor => {
-                  if (contenedor !== contenedorProductos) {
-                      contenedor.innerHTML = ""; // Limpiar contenido existente
-                  }
-              });
-
-              // Ocultar las demás categorías
-              document.querySelectorAll('.categoria').forEach(categoria => {
-                  if (!categoria.querySelector(`[data-${opcion}]`)) {
-                      categoria.querySelector('.texto-categoria').style.display = 'none';
-                      categoria.querySelector('.productos').innerHTML = '';
-                  }
-              });
-          } else {
-              // Si ya se han mostrado todos los productos, redirigir a la página de inicio
-              window.location.href = 'index.html';
+        // Limpiar contenido de las secciones diferentes a la actual
+        document.querySelectorAll(".productos").forEach((contenedor) => {
+          if (contenedor !== contenedorProductos) {
+            contenedor.innerHTML = ""; // Limpiar contenido existente
           }
-      } catch (error) {
-          console.error("Error al obtener los productos:", error);
-          // Manejar el error de manera adecuada
+        });
+
+        // Ocultar las demás categorías
+        document.querySelectorAll(".categoria").forEach((categoria) => {
+          if (!categoria.querySelector(`[data-${opcion}]`)) {
+            categoria.querySelector(".texto-categoria").style.display = "none";
+            categoria.querySelector(".productos").innerHTML = "";
+          }
+        });
+      } else {
+        // Si ya se han mostrado todos los productos, redirigir a la página de inicio
+        window.location.href = "index.html";
       }
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+      // Manejar el error de manera adecuada
+    }
   });
 });
 
-
-  export const  productosInicio = {
-renderInit
-  } 
+export const productosInicio = {
+  renderInit,
+};
