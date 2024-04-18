f9fbd9e44a9a0a5606c2c179be8767a12c85f1e5
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-18 02:47:08 -0300

    actuallizando user definitivo9

--- frontend/lista.controllers.js
+++ frontend/lista.controllers.js
@@ -185,13 +185,199 @@ export class ListaControllers {
         console.error(error);
       }
     });
-  
-  
-  
-  
-  
-  
-
 
   }
 }
+
+
+// export class ListaControllers {
+//   constructor(tabla, titulo) {
+//     this.tabla = tabla;
+//     this.titulo = titulo;
+//     this.listaServicesInstance = new ListaServices();
+//   }
+
+//   async renderLista() {
+//     try {
+//       await this.renderUsersList();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   async renderUsersList() {
+//     const { listado, usersCantidad } = await this.listaServicesInstance.listaUsers();
+//     const { total } = await productoServices.listaProductos();
+
+//     const tituloTabla = `
+//       <div>
+//         <div class="row">
+//           <div class="col-md-12">
+//             <h2 class="card-header">Users</h2>
+//             <table class="table">
+//               <thead>
+//                 <tr>
+//                   <th style="width: 25%;">Users (${usersCantidad})</th>
+//                   <th style="width: 25%;">Create</th>
+//                   <th style="width: 25%;">prod (${total})</th>
+//                   <th style="width: 25%;">Rol</th>
+//                   <th style="width: 25%;">Accion</th>
+//                 </tr>
+//               </thead>
+//             </table>
+//           </div>
+//         </div>
+//       </div>`;
+//     this.titulo.innerHTML = tituloTabla;
+
+//     listado.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+//     for (const usuario of listado) {
+//       const totalProductos = await this.obtenerTotalProductos(usuario._id);
+//       const usuarioData = {
+//         username: usuario.username,
+//         created_at: usuario.created_at,
+//         role: usuario.role,
+//         totalProductos: totalProductos,
+//         id: usuario._id,
+//       };
+//       this.tabla.appendChild(this.nuevaLista(usuarioData));
+//     }
+//   }
+
+//   nuevaLista({ username, created_at, role, totalProductos, id }) {
+//     const fechaCreacion = new Date(created_at);
+//     const fechaFormateada = `${fechaCreacion.getFullYear().toString().slice(-2)}-${("0" + (fechaCreacion.getMonth() + 1)).slice(-2)}-${("0" + fechaCreacion.getDate()).slice(-2)}`;
+
+//     const card = document.createElement("div");
+
+//     card.innerHTML = `
+//       <div class="row">
+//         <div class="col-md-12">
+//           <table class="table">
+//             <tbody>
+//               <tr style="text-align: left;">
+//                 <td style="width: 25%;" >${username}</td>
+//                 <td style="width: 25%;">${fechaFormateada}</td>
+//                 <td style="width: 25%;">${totalProductos}</td>
+//                 <td style="width: 25%;">${role}</td>
+//                 <td style="width: 15%;"><button type="button" class="btn btn-danger" data-userid="${id}" >del</button></td>
+//                 <td style="width: 15%;"><button type="button" class="btn btn-primary" data-userUp="${id}" >up</button></td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>`;
+
+//     card.querySelector("button").addEventListener("click", this.deleteButtonHandler.bind(this));
+//     card.querySelector("[data-userUp]").addEventListener("click", this.updateButtonHandler.bind(this));
+
+//     return card;
+//   }
+
+//   async deleteButtonHandler(event) {
+//     event.preventDefault();
+//     const userId = event.target.dataset.userid;
+
+//     const confirmacion = confirm("¿Estás seguro de que quieres eliminar esta tarjeta?");
+
+//     if (confirmacion) {
+//       try {
+//         const role = await this.getRole(userId);
+//         if (role !== "admin") {
+//           await this.listaServicesInstance.eliminarUser(userId);
+//           event.target.closest(".row").remove();
+//         } else {
+//           alert("No se puede eliminar un usuario administrador");
+//         }
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//   }
+
+//   async updateButtonHandler(event) {
+//     event.preventDefault();
+//     const userId = event.target.dataset.userid;
+//     try {
+//       const username = await this.getUsername(userId);
+//       this.editarLista(username, userId);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   async obtenerTotalProductos(userId) {
+//     const { cantidad } = await this.listaServicesInstance.totalProductos(userId);
+//     return cantidad;
+//   }
+
+//   async getUsername(userId) {
+//     const user = await this.listaServicesInstance.getUser(userId);
+//     return user.username;
+//   }
+
+//   async getRole(userId) {
+//     const user = await this.listaServicesInstance.getUser(userId);
+//     return user.role;
+//   }
+
+//   editarLista(username, id) {
+//     modalControllers.baseModal();
+//     const modal = document.getElementById("modal");
+//     const productoEdicion = modal.querySelector("[data-table]");
+
+//     productoEdicion.innerHTML = `
+//       <div class="text-center">
+//         <div class="card-header">
+//           <form action="/api/updateUser/" enctype="multipart/form-data" id="form"  method="PUT" data-forma>                
+//             <p class="parrafo">Usuario a editar</p>
+//             <div class="form-group">
+//               <input class="form-control mt-3 p-2"  placeholder="Nombre de usuario" type="text" value="${username}" required name="newUsername">
+//             </div>
+//             <div class="form-group"> 
+//               <input class="form-control mt-3 mb-3 p-2"  placeholder="newPassword" type="password"  required name="newPassword">
+//             </div>
+//             <div class="form-check">
+//               <input class="form-check-input" type="checkbox" value="user" name="newRole" id="roleUser" checked>
+//               <label class="form-check-label" for="roleUser">
+//                 user
+//               </label>
+//             </div>
+//             <div class="form-check">
+//               <input class="form-check-input" type="checkbox" value="admin" name="newRole" id="roleAdmin">
+//               <label class="form-check-label" for="roleAdmin">
+//                 admin
+//               </label>
+//             </div>
+//             <div>
+//               <button type="submit" class="btn btn-primary btn-lg">Editar usuario</button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     `;
+
+//     productoEdicion.classList.add("modalVisor");
+
+//     modal.querySelector("[data-forma]").addEventListener("submit", this.formSubmitHandler.bind(this));
+//   }
+
+//   async formSubmitHandler(event) {
+//     event.preventDefault();
+
+//     const form = document.getElementById("form");
+//     const formData = new FormData(form);
+//     const jsonData = {};
+//     for (const [key, value] of formData.entries()) {
+//       jsonData[key] = value;
+//     }
+
+//     try {
+//       await this.listaServicesInstance.updateUser(jsonData, jsonData.id);
+//       modalControllers.modalProductoEditado();
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// }
