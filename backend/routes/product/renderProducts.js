5213fd15242db4ff3ce6ac20c0c22b25ac4c8536
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-04-21 02:58:33 -0300

    agregando clases a los demas controladores 5

--- backend/routes/product/renderProducts.js
+++ backend/routes/product/renderProducts.js
@@ -5,13 +5,13 @@ import Vista from "../../models/Vista.js";
 
 const renderProducts = async (req, res) => {
     try {
-        // //Verificar si el usuario está autenticado
-        // if (!req.isAuthenticated()) {
-        //     return res.status(401).json({ error: 'Usuario no autenticado' });
-        // }
+        //Verificar si el usuario está autenticado
+        if (!req.isAuthenticated()) {
+            return res.status(401).json({ error: 'Usuario no autenticado' });
+        }
       
         //Relacionar id de usuario con producto para visualizar solo sus productos
-        const user_id = req.params.id;
+        const user_id = req.user._id;
         
         // Conectar a la base de datos mediante serverless function
         await mongoose.connect(MONGODB_URI, {
@@ -21,26 +21,26 @@ const renderProducts = async (req, res) => {
 
          //Verificar si el usuario esta autenticado
 
-    //    const usuarioHaIniciadoSesion = req.isAuthenticated() 
-    //    const usuarioAdmin = req.user.role === 'admin' 
-    //   console.log(usuarioAdmin)
+       const usuarioHaIniciadoSesion = req.isAuthenticated() 
+       const usuarioAdmin = req.user.role === 'admin' 
+      console.log(usuarioAdmin)
 
 
 
         let products;
-        // //Verificar si el usuario es administrador
-        // if (req.user.role === 'admin') {
+        //Verificar si el usuario es administrador
+        if (req.user.role === 'admin') {
             // Si es administrador, buscar productos utilizando el modelo Vista
             products = await Vista.find({ user_id: user_id });
-    //    } else {
-    //     // Si no es administrador, buscar productos utilizando el modelo Product
-    //        products = await Product.find({ user_id: user_id });
-    //     }
+       } else {
+        // Si no es administrador, buscar productos utilizando el modelo Product
+           products = await Product.find({ user_id: user_id });
+        }
 
-    //     const totalProduct = await Product.countDocuments();
-    //     const totalVista = await Vista.countDocuments()
+        const totalProduct = await Product.countDocuments();
+        const totalVista = await Vista.countDocuments()
 
-        res.json({ products});
+        res.json({usuarioAdmin,usuarioHaIniciadoSesion, products, total: totalProduct + totalVista});
     } catch (error) {
         console.error('Error al cargar productos:', error);
         res.status(500).json({ error: 'Error al cargar productos' });
